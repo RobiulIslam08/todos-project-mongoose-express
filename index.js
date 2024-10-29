22c9112dd19fa1e79d7d5a6622371ffa1b431560
Robiul Islam <robiulislamrobi0874@gmail.com>
2024-10-29 15:56:05 +0600

    data post and get use mongooses

--- index.js
+++ index.js
@@ -1,30 +1,58 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
 const port = 3000
-
-
+app.use(express.json())
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://todos_user:FywoiOiOhauhJCqh@cluster0.rqcbidk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://todos_user:FywoiOiOhauhJCqh@cluster0.rqcbidk.mongodb.net/todoDB?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+// lagbe nah cause use mongoose
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+const todoSchema = new mongoose.Schema({
+  todo: String,
+  piority: String
 });
+const Todo = mongoose.model('Todo', todoSchema);
 
 async function run() {
   try {
+	  // await client.connect();
+    await mongoose.connect(uri);
+    
+	// const todosCollection = client.db('todoDB').collection('todos')
+
+
+	//get todos data 
+	app.get('/todos',async(req, res)=>{
+		// const todosData = await todosCollection.find({}).toArray()
+		// console.log(todosData)
+    const todos = await Todo.find({})
+		res.send(todos)
+	})
+
+	//insert todos data
+	app.post('/todo-insert',async (req, res)=>{
+		const todoData = req.body
+    const todo = Todo.create(todoData)
+  
+		// const insertData = await todosCollection.insertOne(todoData)
+		res.send(todo)
+	})
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
